428d6f828feaa6d0d1339a690b443d605c9d1c9c
Tefty <tom123other@hotmail.com>
2023-03-03 20:49:41 +0000

    Update Azure-Shutdown-Startup-Tag.ps1

--- Azure-Shutdown-Startup-Tag.ps1
+++ Azure-Shutdown-Startup-Tag.ps1
@@ -7,48 +7,51 @@ $subscriptions = @"
 ]
 "@
 
+    
+# Get the current time and format it to HH:mm
+$currentTime = Get-Date
+       
 foreach ($subscription in $subscriptions) {
 
  Set-AzContext -SubscriptionId $subscription 
 
     # Get all Virtual Machines with the tag "ShutdownTime"
+    $vmsOnSchedule = Get-AzVM -status | Where-Object { ($_.Tags.Keys -contains "ShutdownSchedule") }
     $vmsShutdown = Get-AzVM -status | Where-Object { ($_.Tags.Keys -contains "ShutdownTime") -and ($_.PowerState -eq 'VM running') }
     $vmsStartup = Get-AzVM -status  | Where-Object { ($_.Tags.Keys -contains "StartTime") -and ($_.PowerState -eq 'VM deallocated') }
     # Loop through each VM
-    foreach ($vm in $vmsShutdown) {
-        # Get the value of the ShutdownTime tagVM deallocated
-        $shutdownTime = [DateTime]::ParseExact($vm.Tags["ShutdownTime"], "HH:mm", $null)
-        #$shutdownTime = $vm.Tags["ShutdownTime"]
-    
-        # Get the current time and format it to HH:mm
-        $currentTime = Get-Date
-        $timeDiff = ($currentTime - $shutdownTime).TotalMinutes
-    
-        # Compare the value of the tag with the current time
-        if ($timeDiff -le 80 -and $timeDiff -ge 0) {
+    foreach ($vm in $vmsOnSchedule) {
+        
+       # Shutdown?
+       if($vm.Tags.Contains["ShutdownTime"] && $vm.PowerState == 'running')
+       {
+         # Shut this machine down
+         $shutdownTime = [DateTime]::ParseExact($vm.Tags["ShutdownTime"], "HH:mm", $null)
+         $timeDiff = ($currentTime - $shutdownTime).TotalMinutes
+         if ($timeDiff -le 80 -and $timeDiff -ge 0) {
             # Shut down the VM
             Write-Output "Shutting down VM $($vm.Name) in $($vm.ResourceGroupName) because ShutdownTime tag value is $($shutdownTime)."
             Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
 
         }
-
-    }  
-
-    foreach ($vm in $vmsStartup) {
-        # Get the value of the ShutdownTime tag
-        $startupTime = [DateTime]::ParseExact($vm.Tags["StartTime"], "HH:mm", $null)
-        #$shutdownTime = $vm.Tags["ShutdownTime"]
-    
-        # Get the current time and format it to HH:mm
-        $currentTime = Get-Date
-        $timeDiff = ($currentTime - $startupTime).TotalMinutes
-    
-        # Compare the value of the tag with the current time
-        if ($timeDiff -le 60 -and $timeDiff -ge 0) {
+         continue;
+       }
+                    
+       # Startup?       
+       if($vm.Tags.Contains["StartTime"] && $vm.PowerState == 'deallocated')
+       {
+         # Start this machine up
+         $startupTime = [DateTime]::ParseExact($vm.Tags["StartTime"], "HH:mm", $null)
+         $timeDiff = ($currentTime - $startupTime).TotalMinutes
+         $validDay = !$vm.Tags.Contains["StartTime"] || $vm.Tags["StartTime"] == 'false' || [int]$currentTime.DayOfWeek -le 4;
+         if ($validDay -and $timeDiff -le 60 -and $timeDiff -ge 0) {
             # Shut down the VM
             Write-Output "Starting VM $($vm.Name) in $($vm.ResourceGroupName) because StartupTime tag value is $($startupTime)."
             Start-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
 
         }
-    }
+         continue;
+       }
+
+    } 
 }
